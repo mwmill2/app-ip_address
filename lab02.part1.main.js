95fa0e0b24761572ad04f88feb7d7e4e9066deda
mwmill2 <matt.miller@centurylink.com>
2021-01-22 16:48:03 +0000

    Lab 2 part 1 of 2

--- lab02.part1.main.js
+++ lab02.part1.main.js
@@ -16,10 +16,7 @@ const IPCIDR = require('ip-cidr');
 function getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
-  let firstIpAddress = {
-      ipv4: null,
-      ipv6: null
-  };    
+  let firstIpAddress = null;
   let callbackError = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
@@ -40,8 +37,11 @@ function getFirstIpAddress(cidrStr, callback) {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
-    firstIpAddress.ipv6 = getIpv4MappedIpv6Address(firstIpAddress.ipv4);
   }
+  // Call the passed callback function.
+  // Node.js convention is to pass error data as the first argument to a callback.
+  // The IAP convention is to pass returned data as the first argument and error
+  // data as the second argument to the callback function.
   return callback(firstIpAddress, callbackError);
 }
 
@@ -94,6 +94,10 @@ function getIpv4MappedIpv6Address(ipv4) {
   return ipv6Address;
 }
 
+/*
+  This section is used to test function and log any errors.
+  We will make several positive and negative tests.
+*/
 /*
   This section is used to test function and log any errors.
   We will make several positive and negative tests.
